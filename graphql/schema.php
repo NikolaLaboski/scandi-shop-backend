8dbc94d1fd9e6ba5b646cbf91576ce16918cf812
Nikola Laboski <nikolchelaboski.dev@gmail.com>
2025-08-10 23:32:02 +0200

    finally

--- graphql/schema.php
+++ graphql/schema.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../schemas/ProductSchema.php';
 require_once __DIR__ . '/../mutations/CreateOrderMutation.php';
 
 
-
+require_once __DIR__ . '/../schemas/OrderInputSchema.php';
 
 $mutationFields = CreateOrderMutation::getMutation(); // ['createOrder' => [...] ]
 
